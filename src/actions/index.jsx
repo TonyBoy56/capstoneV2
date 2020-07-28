86562c1834bc3bef4e5fcb983b0e14feb8ba460a
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-28 10:40:55 -0700

    begin adding data to retrieve api call.

---
+++ src/actions/index.jsx
@@ -0,0 +1,24 @@
+import * as c from './ActionsTypes';
+
+export const requestLogin = () => ({
+  type: c.REQUEST_LOGIN
+});
+
+export const getLoginFailure = () => ({
+  type: c.GET_LOGIN_FAILURE,
+  error
+});
+
+export const getLoginSuccess = () => ({
+  type: c.GET_LOGIN_SUCCESS,
+  login
+});
+
+export const makeApiCallLogin = () => {
+  return dispatch => {
+    dispatch(requestLogin);
+    return (
+      fetch
+    );
+  }
+}
